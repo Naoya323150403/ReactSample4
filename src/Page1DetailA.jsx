433e9353c55bc77123e78c8d0683b23857ff8eef
Naoya Teraoka <59727299+Naoya323150403@users.noreply.github.com>
2022-12-23 00:57:44 +0900

    2022/12/22

--- src/Page1DetailA.jsx
+++ src/Page1DetailA.jsx
@@ -1,11 +1,17 @@
 import { useLocation } from "react-router-dom";
+import { useHistory } from "react-router-dom";
+
 export const Page1DetaiA = () => {
   const location = useLocation();
   console.log(location);
+  const history = useHistory();
+
+  const onClickBack = () => history.goBack();
+
   return (
     <div>
       <h1>Page1Detailページです</h1>
-      con
+      <button onClick={onClickBack}>戻る</button>
     </div>
   );
 };
